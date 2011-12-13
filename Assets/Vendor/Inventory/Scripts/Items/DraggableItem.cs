f5ccb525c1c4ec5f979459171d204da1d550be30
Stephen Foster <stephenfoster@Stephen-Fosters-MacBook-Pro.local>
2011-12-12 21:08:20 -0800

    first commit

---
+++ Assets/Vendor/Inventory/Scripts/Items/DraggableItem.cs
@@ -0,0 +1,48 @@
+/* See the copyright information at https://github.com/srfoster/Inventory/blob/master/COPYRIGHT */
+using UnityEngine;
+using System.Collections;
+
+public class DraggableItem : Item {
+	
+	public override void ActiveInInventory()
+	{
+		if(Input.GetMouseButton(0))
+		{
+			Drag();
+		} else {
+			Drop();
+		}
+	}
+	
+	void Drag()
+	{
+		SetHidden(true);
+		getInventory().SetDragged(gameObject);
+	}
+	
+	void Drop()
+	{
+		SetActive(false);
+		getInventory().SetDragged(null);
+
+
+	    if(getInventory().MouseOverInventory()) // If we're back in the inventory
+	    {
+        	SetHidden(false);
+        }else{ // If we're in the world
+        	Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+	    	RaycastHit rayhit = new RaycastHit();
+	        if(Physics.Raycast(ray,out rayhit))
+	        	DroppedOn(rayhit.transform.gameObject);
+	        else
+	        	DroppedOn(null);
+	     }
+	}
+	
+	
+	virtual public void DroppedOn(GameObject gameObject)
+	{
+		SetHidden(false);
+	}
+		
+}
